565afdd9b56c9c7faa82449d948c5e187df9b941
OrDush <oravenie@gmail.com>
2021-12-31 15:40:00 +0200

    Add tests

---
+++ internal/mediator/mediators_test.go
@@ -0,0 +1,31 @@
+package mediator
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type mType struct {
+	test string
+}
+
+var mtr Observable[mType, string]
+
+func init() {
+	mtr = New[mType, string]()
+}
+
+func TestMediator(t *testing.T) {
+	x := func(mp *MediatePayload[mType, string]) {
+		assert.Equal(t, mp.Payload.test, "test?")
+		mp.Response = "yay"
+	}
+	mediator := mtr.NewMediator("add", x)
+	// need to create locker for obs and subscribers
+	time.Sleep(1 * time.Second)
+	resp := mediator.Mediate(mType{test: "test?"})
+	assert.Equal(t, resp, "yay")
+
+}
