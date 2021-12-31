645d966dca3d9e52366628be833ff39ccc8603e8
OrDush <oravenie@gmail.com>
2021-12-31 15:13:50 +0200

    internal folder

---
+++ internal/mediator/observable.go
@@ -0,0 +1,40 @@
+package mediator
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Input interface {
+}
+type Output interface {
+}
+type Observable[T Input, K Output] struct {
+	time    time.Time
+	args    T
+	name    string
+	sitters map[string][]chan eventMessage[T, K]
+}
+type eventMessage[T Input, K Output] struct {
+	withresponse  bool
+	CorrelationId uuid.UUID
+	Args          T
+	response      K
+}
+
+func New[T Input, K Output]() Observable[T, K] {
+	return Observable[T, K]{time: time.Now()}
+}
+func (obs *Observable[T, K]) Handle(ctx context.Context) {
+	log.Printf("deleting: %+v\n", obs)
+
+}
+func (obs *Observable[T, K]) Subscriber(action string) chan eventMessage[T, K] {
+	ch := make(chan eventMessage[T, K])
+	obs.AddSitter(action, ch)
+	return ch
+
+}
